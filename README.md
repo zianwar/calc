28441163449f764096215f84db80a4f447dab226
Anwar Ziani <ziawnar@Macbook.fritz.box>
2017-02-26 02:07:15 +0100

    Init

---
+++ README.md
@@ -0,0 +1,43 @@
+# calc
+Command line utility for advanced math operations built with [Go](https://golang.org), uses the [https://newton.now.sh/](https://newton.now.sh/) Math API under the hood.
+
+### Installation:
+1. Make sure you have [Go](http://golang.org/doc/install.html) installed
+2. Install calc:
+```
+$ go get github.com/Zianwar/calc
+```
+
+### Usage:
+
+```
+Usage: calc [operation] [expression]
+
+  • Where operation is defined as:
+
+	 derive:     Derives expression.        eg:    x^2 + 2x   =>             2x + 2
+	 zeroes:     Finds expression zeros.    eg:    x^2 + 2x   =>            [-2, 0]
+	 cos:        Cosine of number.          eg:          pi   =>                 -1
+	 sin:        Sine of number.            eg:           0   =>                  0
+	 arccos:     Inverse Cosine of number   eg:           1   =>                  0
+	 arctan:     Inverse Tangent of number  eg:           0   =>                  0
+	 abs:        Absolute Value of number   eg:          -1   =>                  1
+	 factor:     Factorise expression.      eg:    x^2 + 2x   =>           x(x + 2)
+	 integrate:  integrates expression.      eg:    x^2 + 2x   =>  1/3 x^3 + x^2 + C
+	 tan:        Tangent of number.         eg:           0   =>                  0
+	 arcsin:     Inverse Sine of number     eg:           0   =>                  0
+	 log:        Logarithm                  eg:         218   =>                  3
+	 simplify:   Simplifies expression.     eg:  2^2 + 2(2)   =>                  8
+
+  • And expression could be any number or mathematical equation.
+```
+
+### Examples
+
+```
+$ calc integrate "x"
+1/2 x^2
+
+$ calc simplify "1+2-2"
+1
+```
---
+++ calc.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+var operations = map[string]string{
+	"simplify":  ` Simplifies expression.     eg:  2^2 + 2(2)   =>                  8`,
+	"factor":    `   Factorise expression.      eg:    x^2 + 2x   =>           x(x + 2)`,
+	"derive":    `   Derives expression.        eg:    x^2 + 2x   =>             2x + 2`,
+	"integrate": `integrates expression.     eg:    x^2 + 2x   =>  1/3 x^3 + x^2 + C`,
+	"zeroes":    `   Finds expression zeros.    eg:    x^2 + 2x   =>            [-2, 0]`,
+	"cos":       `      Cosine of number.          eg:          pi   =>                 -1`,
+	"sin":       `      Sine of number.            eg:           0   =>                  0`,
+	"tan":       `      Tangent of number.         eg:           0   =>                  0`,
+	"arccos":    `   Inverse Cosine of number   eg:           1   =>                  0`,
+	"arcsin":    `   Inverse Sine of number     eg:           0   =>                  0`,
+	"arctan":    `   Inverse Tangent of number  eg:           0   =>                  0`,
+	"abs":       `      Absolute Value of number   eg:          -1   =>                  1`,
+	"log":       `      Logarithm                  eg:         218   =>                  3`,
+}
+
+func main() {
+	if l := len(os.Args[1:]); l < 2 {
+		usage()
+	}
+
+	op, rest := os.Args[1], os.Args[2:]
+
+	if _, ok := operations[strings.Replace(op, " ", "", -1)]; !ok {
+		fmt.Print("\"" + op + "\" operation not recognized.")
+	}
+
+	expression := strings.Replace(strings.Join(rest, " "), " ", "", -1)
+
+	resp, err := http.Get("https://newton.now.sh/" + op + "/" + expression)
+	fatal(err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	fatal(err)
+
+	defer resp.Body.Close()
+
+	var result map[string]string
+	json.Unmarshal([]byte(body), &result)
+	fmt.Println(result["result"])
+}
+
+func fatal(err interface{}) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func usage() {
+	bold := color.New(color.Bold)
+	green := color.New(color.FgGreen)
+
+	fmt.Printf("\nUsage: %s [operation] [expression]\n\n", bold.Sprint(green.Sprint("calc")))
+	fmt.Print("  • Where operation is defined as:\n\n")
+	for cmd, desc := range operations {
+		fmt.Printf("\t %s:  %s\n", cmd, desc)
+	}
+	fmt.Print("\n  • And expression could be number or any mathematical equation.\n\n")
+	os.Exit(1)
+}
